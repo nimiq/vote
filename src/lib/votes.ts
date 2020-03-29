dea94c7f3af269b4c85326a4891b4ab332c86c00
Sven <sven@nimiq.com>
2020-03-29 22:52:18 +0800

    formatting and sanitization

--- src/lib/votes.ts
+++ src/lib/votes.ts
@@ -1,10 +1,13 @@
+import { unique } from 'typescript-array-utils';
+import { LimitIterable } from '@nimiq/core-web';
+
 export enum VoteTypes {
-    yesNo,
-    multipleChoice,
-    weightedChoices,
+    yesNo = 'yesNo',
+    multipleChoice = 'multipleChoice',
+    weightedChoices = 'weightedChoices',
 }
 
-type BaseVote = {
+export type BaseVote = {
     name: string,
 }
 
@@ -13,27 +16,34 @@ export enum YesNo {
     yes = 'yes',
 }
 
-type YesNoVote = BaseVote & {
-    answer: YesNo;
+export type YesNoVote = BaseVote & {
+    answer: YesNo,
 }
 
-type MultipleChoiceVote = BaseVote & {
+export type MultipleChoiceVote = BaseVote & {
     choices: string[],
 }
 
-type WeightedCoicesVote = BaseVote & {
+export type WeightedCoicesVote = BaseVote & {
     choices: WeightedChoice[],
 }
 
-type WeightedChoice = {
+export type WeightedChoice = {
     name: string,
     weight: number,
 }
 
-export function parseVote(message: string, type: VoteTypes.yesNo): YesNoVote
-export function parseVote(message: string, type: VoteTypes.multipleChoice): MultipleChoiceVote
-export function parseVote(message: string, type: VoteTypes.weightedChoices): WeightedCoicesVote
-export function parseVote(message: string, type: VoteTypes): YesNoVote | MultipleChoiceVote | WeightedCoicesVote {
+export function parseVote(message: string, type: VoteTypes.yesNo): YesNoVote;
+export function parseVote(message: string, type: VoteTypes.multipleChoice): MultipleChoiceVote;
+export function parseVote(message: string, type: VoteTypes.weightedChoices): WeightedCoicesVote;
+export function parseVote(message: string, type: string): BaseVote;
+export function parseVote(
+    message: string,
+    type: VoteTypes | string,
+): YesNoVote | MultipleChoiceVote | WeightedCoicesVote {
+    function invalid(reason: string) {
+        throw new Error(`Invalid vote: "${message}" is not a valid "${type}" vote. Reason: ${reason}.`);
+    }
     if (message.startsWith('Vote')) {
         // Human readable version
         try {
@@ -41,48 +51,71 @@ export function parseVote(message: string, type: VoteTypes): YesNoVote | Multipl
             const elements = message.split('_');
             elements.shift();
             const name = elements.shift()!;
-            switch(type) {
-                case VoteTypes.yesNo: return { name, answer: elements[0] == "yes" ? YesNo.yes : YesNo.no }
-                case VoteTypes.multipleChoice: return { name, choices: elements }
-                case VoteTypes.weightedChoices: {
-                    const choices: WeightedChoice[] = elements.map((option => {
-                        const [name, weight] = option.split(':');
-                        return { name, weight: parseInt(weight, 10) };
-                    }));
-                    return { name, choices }
+            switch (type) {
+                case VoteTypes.yesNo: {
+                    if (elements.length !== 1) invalid('only one answer expected');
+                    const answer = elements[0];
+                    if (answer === 'yes' || answer === 'no') invalid('only yes or no expected');
+                    return { name, answer: elements[0] === 'yes' ? YesNo.yes : YesNo.no };
                 }
-                default: throw `Vote type "${type}" does not exist`;
+                case VoteTypes.multipleChoice: {
+                    if (elements.length !== unique(elements).length) invalid('choices must be unique');
+                    return { name, choices: elements };
+                }
+                case VoteTypes.weightedChoices: {
+                    const choices: WeightedChoice[] = elements.map(((option) => {
+                        const [name2, weight] = option.split(':');
+                        return { name: name2, weight: parseInt(weight, 10) };
+                    }));
+                    if (choices.length !== unique(choices, (a, b) => a.name === b.name).length) {
+                        invalid('choices must be unique');
+                    }
+                    if (choices.filter((choice) => choice.weight < 0 || choice.weight > 99).length > 0) {
+                        invalid('choice weights must be between 0 and 99 (inclusive).');
+                    }
+                    return { name, choices };
+                }
+                default: throw new Error(`Vote type "${type}" does not exist`);
             }
-        }
-        catch (e) {
-            // TODO throw?
-            console.warn('Parsing vote failed', message, type, e);
+        } catch (e) {
+            throw new Error(`Parsing vote failed. Reason: ${e}`);
         }
     }
-    throw `Not a voting message or format not supported "${message}"`;
+    throw new Error(`Not a voting message or format not supported "${message}"`);
 }
 
-export function serializeVote(vote: YesNoVote, type: VoteTypes.yesNo, prefix: string): string
-export function serializeVote(vote: MultipleChoiceVote, type: VoteTypes.multipleChoice, prefix: string): string
-export function serializeVote(vote: WeightedCoicesVote, type: VoteTypes.weightedChoices, prefix: string): string
-export function serializeVote(vote: YesNoVote | MultipleChoiceVote | WeightedCoicesVote, type: VoteTypes, prefix = 'Vote'): string {
-    if (prefix == 'Vote') {
-        return `${prefix}_${vote.name}_${serializeChoice(vote, type, prefix)}`;
-    }
-    throw `Format "${prefix} not supported.`;
-}
-
-function serializeChoice(vote: YesNoVote | MultipleChoiceVote | WeightedCoicesVote, type: VoteTypes, prefix = 'Vote'): string {
-    if (prefix == 'Vote') {
-        switch(type) {
-            case VoteTypes.yesNo: return (vote as YesNoVote).answer
-            case VoteTypes.multipleChoice: return (vote as MultipleChoiceVote).choices.join('_')
+function serializeChoice(
+    vote: YesNoVote | MultipleChoiceVote | WeightedCoicesVote,
+    type: VoteTypes,
+    prefix = 'Vote',
+): string {
+    if (prefix === 'Vote') {
+        switch (type) {
+            case VoteTypes.yesNo: return (vote as YesNoVote).answer;
+            case VoteTypes.multipleChoice: return (vote as MultipleChoiceVote).choices.join('_');
             case VoteTypes.weightedChoices: return (vote as WeightedCoicesVote).choices
                 .map((choice) => `${choice.name}:${choice.weight}`)
                 .join('_');
-            default: throw `Vote type "${type}" does not exist`;
+            default: throw new Error(`Vote type "${type}" does not exist`);
         }
-        return `${prefix}_${this.name}_${this.serializeOptions(prefix)}`;
     }
-    throw `Format "${prefix} not supported.`;
+    throw new Error(`Format "${prefix}" not supported.`);
+}
+
+export function serializeVote(vote: YesNoVote, type: VoteTypes.yesNo, prefix?: string): string
+export function serializeVote(vote: MultipleChoiceVote, type: VoteTypes.multipleChoice, prefix?: string): string
+export function serializeVote(vote: WeightedCoicesVote, type: VoteTypes.weightedChoices, prefix?: string): string
+export function serializeVote(
+    vote: YesNoVote | MultipleChoiceVote | WeightedCoicesVote,
+    type: VoteTypes,
+    prefix = 'Vote',
+): string {
+    if (prefix === 'Vote') {
+        return `${prefix}_${vote.name}_${serializeChoice(vote, type, prefix)}`;
+    }
+    throw new Error(`Format "${prefix} not supported.`);
+}
+
+export function voteTotalWeight(choices: WeightedChoice[]) {
+    return choices.map((choice) => choice.weight).reduce((previous, current) => previous + current);
 }
