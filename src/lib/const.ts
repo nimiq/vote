f3da05736ab70b0d62afcce75292fe401f61eead
Sven <sven@nimiq.com>
2020-04-03 17:26:19 +0800

    naming

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -2,5 +2,5 @@ export const testnet = true;
 export const dummies = false;
 export const debug = false;
 export const voteAddress = 'NQ07 0000 0000 0000 0000 0000 0000 0000 0000';
-export const votingLocation = 'https://nimiq.community/votings.';
+export const votingLocation = 'https://nimiq.community/voting.';
 export const configAddress = `${votingLocation}${testnet ? 'test' : 'live'}.json`;
