d70e2d9738a366037b92b7f1b12820aa9bec234a
Sven <sven@nimiq.com>
2020-04-14 11:19:06 +0800

    simplify

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -7,4 +7,4 @@ export const testnet = !production;
 export const dummies = useDummies && !production;
 export const debug = showDebugging && !production;
 export const votingLocation = 'https://nimiq.community/voting.';
-export const configAddress = `${votingLocation}${specialConfig ? specialConfig : production ? 'live' : 'test'}.json`;
+export const configAddress = `${votingLocation}${specialConfig || (production ? 'live' : 'test')}.json`;
