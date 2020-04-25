823556af7c092d93cf735fffc1e020cc8baa5e4f
Sven <sven@nimiq.com>
2020-04-25 22:18:39 +0800

    moving configs to nimiq.com

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -6,6 +6,6 @@ const showDebugging = false;
 export const testnet = !production;
 export const dummies = useDummies && !production;
 export const debug = showDebugging && !production;
-export const votingLocation = 'https://nimiq.community/voting.';
+export const votingLocation = 'https://nimiq.com/vote/config/';
 export const configAddress = `${votingLocation}${specialConfig || (production ? 'live' : 'test')}.json`;
 export const contactInfo = 'Please contact Sven at sven@nimiq.com or via Telegram @svubx.';
